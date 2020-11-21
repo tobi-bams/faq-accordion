c25fd6210e0ddf1ec86b8abd1579bd6e5d1a5d54
tobi-bams <teebams49@gmail.com>
2020-11-21 17:24:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let questions = document.querySelectorAll(".question");
+// let me = document.getElementById("me");
+
+// me.parentElement.nextElementSibling
+
+
+questions.forEach((question) => {
+    let rotateValue = "180deg"
+    question.addEventListener("click", (evt)=> {
+        if(rotateValue  === "180deg"){
+            evt.target.parentElement.nextElementSibling.style.display = "flex";
+            evt.target.nextElementSibling.style.transform = `rotate(${rotateValue })`;
+            rotateValue  = "0deg";
+        }
+        
+        else {
+            evt.target.parentElement.nextElementSibling.style.display = "none";
+            evt.target.nextElementSibling.style.transform = `rotate(${rotateValue })`;
+            rotateValue  = "180deg";
+        }
+        
+    })
+})
