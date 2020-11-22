1c93f94fe97787a3f50e98d145d8d1320b8d28ac
tobi-bams <teebams49@gmail.com>
2020-11-22 23:57:43 +0100

    mobile view

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let questions = document.querySelectorAll(".question");
 // let me = document.getElementById("me");
 
-// me.parentElement.nextElementSibling
+// me.style.fontWeight = "700"
 
 
 questions.forEach((question) => {
@@ -11,14 +11,18 @@ questions.forEach((question) => {
             questions.forEach((question) => {
                 question.parentElement.nextElementSibling.style.display = "none";
                 question.nextElementSibling.style.transform = `rotate(0deg)`;
+                question.style.fontWeight = "400";
+
             })
             evt.target.parentElement.nextElementSibling.style.display = "flex";
-            evt.target.nextElementSibling.style.transform = `rotate(${rotateValue })`;
+            evt.target.nextElementSibling.style.transform = `rotate(180deg)`;
+            evt.target.style.fontWeight = "700";
             rotateValue  = "0deg";
         }
         else {
             evt.target.parentElement.nextElementSibling.style.display = "none";
             evt.target.nextElementSibling.style.transform = `rotate(${rotateValue })`;
+            evt.target.style.fontWeight = "400";
             rotateValue  = "180deg";
         }
         
